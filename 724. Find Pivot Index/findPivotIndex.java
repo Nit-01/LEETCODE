299a4401c6dc5682f086913f78962d8608a9c4a7
Nit-01 <niteeshukotihal@gmail.com>
2026-02-25 21:45:50 +0530

    Applied gitignore to remove .class files

---
+++ 724. Find Pivot Index/findPivotIndex.java
@@ -0,0 +1,60 @@
+/*
+if a number given sum of left == right we have to return the middle number index;
+if not equal return index = -1 
+
+
+Given an array of integers nums, calculate the pivot index of this array.
+The pivot index is the index where the sum of all the numbers strictly to the 
+left of the index is equal to the sum of all the numbers strictly to the index's right.
+If the index is on the left edge of the array, then the left sum is 0 because 
+there are no elements to the left. This also applies to the right edge of the array.
+Return the leftmost pivot index. If no such index exists, return -1.
+
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+Example 2:
+
+Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+
+
+ */
+
+import java.util.*;
+
+class findPivotIndex{
+    public static void main(String[]args){
+        Scanner sn=new Scanner(System.in);
+        int n=sn.nextInt();
+        int []nums=new int[n];
+
+        for(int i=0;i<n;i++){
+            nums[i]=sn.nextInt();
+        }
+        int total=0;
+        int leftsum=0;
+        int index=-1;
+
+        for(int num:nums){
+            total+=num;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            total-=nums[i];
+
+            if(total==leftsum){
+            index=i;
+            break;
+        }
+        leftsum+=nums[i];
+        }
+        System.out.println(index);
+        
+    }
+}
