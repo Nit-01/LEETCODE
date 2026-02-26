3a4f4541e86a75c1a1d20b4eca746bedc1c365ea
Nit-01 <niteeshukotihal@gmail.com>
2026-02-26 15:10:31 +0530

    Two Sum

--- 1. Two Sum/TwoSum.java
+++ 1. Two Sum/TwoSum.java
@@ -8,17 +8,44 @@ Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 */
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
+import java.util.*;
 
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
+class twoSum {
 
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i,j};
-                }
+    public static int[] findTwoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int com = target - nums[i];
+
+            if (map.containsKey(com)) {
+                return new int[]{map.get(com), i};
             }
+
+            map.put(nums[i], i);
         }
-        return new int[]{};
+
+        return new int[]{-1, -1};
+    }
+    public static void main(String[] args) {
+
+        Scanner sn = new Scanner(System.in);
+
+        int n = sn.nextInt();
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sn.nextInt();
+        }
+
+        int target = sn.nextInt();
+
+        int[] result = findTwoSum(nums, target);
+
+        System.out.println(result[0] + " " + result[1]);
+
     }
 }
