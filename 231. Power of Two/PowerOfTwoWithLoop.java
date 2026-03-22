34dda11d57b15b10d229d7a31e8281334a61b408
Nit-01 <niteeshukotihal@gmail.com>
2026-03-22 22:56:35 +0530

    Power of 2 check with loop

---
+++ 231. Power of Two/PowerOfTwoWithLoop.java
@@ -0,0 +1,33 @@
+/*
+Example 1:
+
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+Example 2:
+
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: n = 3
+Output: false
+
+*/
+import java.util.*;
+
+public class PowerOfTwoWithLoop {
+    public static void main(String[]args){
+        Scanner sn=new Scanner(System.in);
+        int n=sn.nextInt();
+        System.out.println(equal(n));
+    }
+    public static boolean equal(int n){
+        if (n<=0)return false;
+        while(n%2==0){
+            n=n/2;
+        }
+        return n==1;
+    }
+}
